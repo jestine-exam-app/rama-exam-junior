4824957a3a258ff492b4113d41f4c5e428218b91
jestine-exam-app <jestinenaibei768@gmail.com>
2025-06-09 23:28:36 +0300

    Create firebase-config.js
    Add firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDK-89FetrtEtuYNB7Eblne9-WBK4gO6Jw",
+  authDomain: "rama-exam-system.firebaseapp.com",
+  projectId: "rama-exam-system",
+  storageBucket: "rama-exam-system.firebasestorage.app",
+  messagingSenderId: "445266965804",
+  appId: "1:445266965804:web:d6ad5f78c38af6ec942889"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
