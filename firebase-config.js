7e6a57d34b4c41a6e030747940be887927207cbd
jestine-exam-app <jestinenaibei768@gmail.com>
2025-06-10 10:38:01 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,1 @@
-
-// Firebase configuration placeholder
-const firebaseConfig = {
-  apiKey: "AIzaSyDK-89FetrtEtuYNB7Eblne9-WBK4gO6Jw",
-  authDomain: "rama-exam-system.firebaseapp.com",
-  projectId: "rama-exam-system",
-  storageBucket: "rama-exam-system.appspot.com",
-  messagingSenderId: "445266965804",
-  appId: "1:445266965804:web:d6ad5f78c38af6ec942889"
-};
+// Firebase config placeholder
